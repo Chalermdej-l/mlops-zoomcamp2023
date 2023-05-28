3090dcc4f1631fef7eb5d2a025bfa7691271821e
Chalermdej Lematavekul <chalermdej@avalon-analytics.com>
2023-05-28 20:46:59 +0700

    W2 homework

---
+++ W2/script/train.py
@@ -0,0 +1,30 @@
+import os
+import pickle
+import click
+import mlflow
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment('test')
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+def run_train():
+    print('start running')
+
+    mlflow.sklearn.autolog()
+    
+    X_train, y_train = load_pickle(os.path.join('D:\Avalon_Pratice_Code\ML_Project\DatatalkClub\mlops-zoomcamp2023\W2\output', "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join('D:\Avalon_Pratice_Code\ML_Project\DatatalkClub\mlops-zoomcamp2023\W2\output', "val.pkl"))
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        print(rmse)
+
+if __name__ == '__main__':
+        run_train()
